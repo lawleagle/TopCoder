70ebd65ec6c0fdda02c5455f1fbb1cb8f8bf5c0c
Emanuel T <lawleagle@gmail.com>
2020-06-04 16:44:09 +0300

    SymmetricDetection

---
+++ SRM 713/SymmetryDetection.cpp
@@ -0,0 +1,145 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct SymmetryDetection
+{
+    string detect(vector<string> a)
+    {
+        int horz = 1;
+        for (int i = 0; i < a.size()/2 && horz; ++i)
+        {
+            for (int j = 0; j < a[i].size() && horz; ++j)
+            {
+                if (a[i][j] != a[a.size()-i-1][j])
+                {
+                    horz = 0;
+                }
+            }
+        }
+        int vert = 1;
+        for (int i = 0; i < a.size() && vert; ++i)
+        {
+            for (int j = 0; j < a[i].size()/2 && vert; ++j)
+            {
+                if (a[i][j] != a[i][a[i].size() - j - 1])
+                {
+                    vert = 0;
+                }
+            }
+        }
+
+        if (vert && horz)
+            return "Both";
+        if (vert) return "Vertically symmetric";
+        if (horz) return "Horizontally symmetric";
+        return "Neither";
+    }
+};
+
+// CUT begin
+ifstream data("SymmetryDetection.sample");
+
+string next_line() {
+    string s;
+    getline(data, s);
+    return s;
+}
+
+template <typename T> void from_stream(T &t) {
+    stringstream ss(next_line());
+    ss >> t;
+}
+
+void from_stream(string &s) {
+    s = next_line();
+}
+
+template <typename T> void from_stream(vector<T> &ts) {
+    int len;
+    from_stream(len);
+    ts.clear();
+    for (int i = 0; i < len; ++i) {
+        T t;
+        from_stream(t);
+        ts.push_back(t);
+    }
+}
+
+template <typename T>
+string to_string(T t) {
+    stringstream s;
+    s << t;
+    return s.str();
+}
+
+string to_string(string t) {
+    return "\"" + t + "\"";
+}
+
+bool do_test(vector<string> board, string __expected) {
+    time_t startClock = clock();
+    SymmetryDetection *instance = new SymmetryDetection();
+    string __result = instance->detect(board);
+    double elapsed = (double)(clock() - startClock) / CLOCKS_PER_SEC;
+    delete instance;
+
+    if (__result == __expected) {
+        cout << "PASSED!" << " (" << elapsed << " seconds)" << endl;
+        return true;
+    }
+    else {
+        cout << "FAILED!" << " (" << elapsed << " seconds)" << endl;
+        cout << "           Expected: " << to_string(__expected) << endl;
+        cout << "           Received: " << to_string(__result) << endl;
+        return false;
+    }
+}
+
+int run_test(bool mainProcess, const set<int> &case_set, const string command) {
+    int cases = 0, passed = 0;
+    while (true) {
+        if (next_line().find("--") != 0)
+            break;
+        vector<string> board;
+        from_stream(board);
+        next_line();
+        string __answer;
+        from_stream(__answer);
+
+        cases++;
+        if (case_set.size() > 0 && case_set.find(cases - 1) == case_set.end())
+            continue;
+
+        cout << "  Testcase #" << cases - 1 << " ... ";
+        if ( do_test(board, __answer)) {
+            passed++;
+        }
+    }
+    if (mainProcess) {
+        cout << endl << "Passed : " << passed << "/" << cases << " cases" << endl;
+        int T = time(NULL) - 1591277682;
+        double PT = T / 60.0, TT = 75.0;
+        cout << "Time   : " << T / 60 << " minutes " << T % 60 << " secs" << endl;
+        cout << "Score  : " << 250 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT)) << " points" << endl;
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(2);
+    set<int> cases;
+    bool mainProcess = true;
+    for (int i = 1; i < argc; ++i) {
+        if ( string(argv[i]) == "-") {
+            mainProcess = false;
+        } else {
+            cases.insert(atoi(argv[i]));
+        }
+    }
+    if (mainProcess) {
+        cout << "SymmetryDetection (250 Points)" << endl << endl;
+    }
+    return run_test(mainProcess, cases, argv[0]);
+}
+// CUT end
